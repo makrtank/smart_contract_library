5682d73b2a00d6f0254e56c83f06a6731a8ee4b6
Mark Bilodeau <mark@Marks-MacBook-Pro-14.local>
2022-06-13 12:51:15 -0700

    first commit

---
+++ tests/test_lottery_integration.py
@@ -0,0 +1,24 @@
+from brownie import network, Lottery
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    fund_with_link,
+    get_account,
+)
+import pytest
+from scripts.deploy_lottery import deploy_lottery
+import time
+
+
+def test_can_pick_winner():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee() + 1000})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee() + 1000})
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    time.sleep(120)
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
